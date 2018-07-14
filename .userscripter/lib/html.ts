0d5c559c9d922fb57ade8e4db8060252c88839d8
Simon Alling <alling.simon@gmail.com>
2018-07-14 16:18:37 +0200

    Remove legacy type-checking code
    Such code is unnecessary now that we use TypeScript.

--- .userscripter/lib/html.ts
+++ .userscripter/lib/html.ts
@@ -7,30 +7,6 @@ import { is, isString } from "ts-type-guards";
 
 
 /**
- * Asserts that something is an {@code HTMLElement}.
- * @param {} x
- *
- * @throws {TypeError} The argument must be an {@code HTMLElement}.
- */
-function assert_isHTMLElement(node: any): void {
-    if (!isHTMLElement(node)) {
-        throw new TypeError(`${JSON.stringify(node)} is not an HTML element.`);
-    }
-}
-
-/**
- * Asserts that something is a {@code string}.
- * @param {} x
- *
- * @throws {TypeError} The argument must be a {@code string}.
- */
-function assert_isString(s: any): void {
-    if (!isString(s)) {
-        throw new TypeError(`${JSON.stringify(s)} is not a string.`);
-    }
-}
-
-/**
  * Creates a function that checks if an element has a certain ID.
  * @param {string} id The ID to check for.
  *
@@ -41,11 +17,9 @@ export function hasID(id: string): (element: HTMLElement) => boolean {
      * Checks if an element has a certain ID.
      * @param {HTMLElement} element The element to check.
      *
-     * @throws {TypeError} If {@code element} is not an {@code HTMLElement}.
-     *
      * @return {boolean} Whether {@code element} has the ID in question.
      */
-    return element => (assert_isHTMLElement(element), element.id === id);
+    return element => element.id === id;
 }
 
 /**
@@ -79,22 +53,16 @@ export function existenceChecker(selector: string): () => boolean {
 /**
  * Removes an element from the DOM tree. Intended for use in {@code map}, {@code forEach}, etc.
  * @param {HTMLElement} element The element to remove.
- *
- * @throws {TypeError} The argument must be an {@code HTMLElement}.
  */
 export function remove(element: HTMLElement): void {
-    assert_isHTMLElement(element);
     element.remove();
 }
 
 /**
  * Flushes an element, i.e. removes everything inside it while keeping its attributes etc.
  * @param {HTMLElement} element The element to flush.
- *
- * @throws {TypeError} The argument must be an {@code HTMLElement}.
  */
 export function flush(element: HTMLElement): void {
-    assert_isHTMLElement(element);
     element.innerHTML = "";
 }
 
@@ -103,15 +71,12 @@ export function flush(element: HTMLElement): void {
  * @param {string} css The CSS to insert.
  * @param {string} [id] Optional ID of the inserted {@code <style>} element.
  *
- * @throws {TypeError} The first argument must be a string. The second argument, if passed, must be a string.
  * @throws {Error} If there already is an element with the specified ID.
  */
 export function insertCSS(css: string, id?: string): void {
-    assert_isString(css);
     const styleElement = document.createElement("style");
     styleElement.textContent = css;
-    if (typeof id !== "undefined") {
-        assert_isString(id);
+    if (isString(id)) {
         if (isHTMLElement(byID(id))) {
             throw new Error(`Cannot insert CSS with ID ${JSON.stringify(id)} because there is already an element with that ID.`);
         }
@@ -125,12 +90,9 @@ export function insertCSS(css: string, id?: string): void {
  * @param {string} css The CSS to append.
  * @param {string} [id] ID of the {@code <style>} element to append to.
  *
- * @throws {TypeError} Both arguments must be strings.
  * @throws {Error} If there is no existing {@code <style>} element with the specified ID.
  */
 export function appendCSS(css: string, id: string): void {
-    assert_isString(css);
-    assert_isString(id);
     const styleElement = byID(id);
     if (isHTMLElement(styleElement)) {
         if (styleElement instanceof HTMLStyleElement) {
