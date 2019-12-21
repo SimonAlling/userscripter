3f2da22543cf2bdb75c72a004438d92b97699468
Simon Alling <alling.simon@gmail.com>
2019-12-21 16:00:32 +0100

    Delete old codebase

--- .userscripter/example/userscripter/misc.ts
+++
@@ -1,7 +0,0 @@
-import { isHTMLElement, byID } from "lib/html";
-import * as SITE from "globals-site";
-import * as CONFIG from "globals-config";
-
-export function hasAlreadyRun(): boolean {
-    return isHTMLElement(byID(CONFIG.ID_STYLE_ELEMENT));
-}
--- config/validation/userscript-optional.ts
+++
@@ -1,7 +0,0 @@
-export default [
-    "runAt",
-    "icon",
-    "downloadURL",
-    "updateURL",
-    "hostedAt",
-];
