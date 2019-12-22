b9d42a2839138179e16ee568181d1728c9b7bbf8
Simon Alling <alling.simon@gmail.com>
2019-12-22 18:00:44 +0100

    Add API test for index.ts

--- __tests__/api.test.ts
+++ __tests__/api.test.ts
@@ -1,7 +1,11 @@
+import * as fs from "fs";
+import * as path from "path";
+
 import * as Environment from "../src/lib/environment";
 import * as Errors from "../src/lib/errors";
 import * as Operations from "../src/lib/operations";
 import * as Stylesheets from "../src/lib/stylesheets";
+import * as index from "../src/lib";
 
 it("exposes the intended API", () => {
   const a: Environment.Condition = Environment.ALWAYS;
@@ -21,4 +25,24 @@ it("exposes the intended API", () => {
   expect(Stylesheets.insertStylesheets).toBeDefined();
   expect(Stylesheets.enableStylesheet).toBeDefined();
   expect(Stylesheets.disableStylesheet).toBeDefined();
+});
+
+it("exposes everything in lib in index.ts", done => {
+  fs.readdir(path.resolve(__dirname, "..", "src", "lib"), (err, filenames) => {
+    expect(err).toBeNull();
+    expect(filenames).toEqual([
+      "environment.ts",
+      "errors.ts",
+      "index.ts",
+      "log.ts",
+      "main.ts",
+      "operations.ts",
+      "preferences.ts",
+      "stylesheets.ts",
+    ]);
+    const modulesThatAreExported = Object.keys(index);
+    const modulesThatShouldBeExported = filenames.map(n => n.replace(/\.ts$/, "")).filter(n => n !== "index");
+    expect(modulesThatAreExported).toEqual(modulesThatShouldBeExported);
+    done();
+  });
 });
