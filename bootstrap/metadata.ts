fe2405d4e291be3e27a182898916830b88ecd7e7
Simon Alling <alling.simon@gmail.com>
2019-12-29 00:15:49 +0100

    Add description, author and namespace in metadata

--- bootstrap/metadata.ts
+++ bootstrap/metadata.ts
@@ -9,10 +9,13 @@ export default function(_: BuildConfig): Metadata {
     return {
         name: U.name,
         version: U.version,
+        description: U.description,
+        author: U.author,
         match: [
             `*://${U.hostname}/*`,
             `*://www.${U.hostname}/*`,
         ],
+        namespace: U.namespace,
         run_at: U.runAt,
     };
 }
