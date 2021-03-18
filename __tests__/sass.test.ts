327e179e1d3b308b8636db3df88abd3725f59f98
avallete <avallete@student.42.fr>
2021-03-18 18:34:50 +0100

    test(sass): refactor tests to match the new getGlobal function prototype

--- __tests__/sass.test.ts
+++ __tests__/sass.test.ts
@@ -1,10 +1,11 @@
 import * as CONFIG from "./config-example";
 import { getGlobalFrom } from "../src/build/internal/sass";
+import sass from 'sass'
 
 it("can expose data to SASS", () => {
   const getGlobal = getGlobalFrom({ CONFIG });
-  const height = getGlobal({ getValue: () => "CONFIG.EXAMPLE_HEIGHT" });
-  const fontSize = getGlobal({ getValue: () => "CONFIG.EXAMPLE_FONT_SIZE" });
+  const height = getGlobal(new sass.types.String("CONFIG.EXAMPLE_HEIGHT"));
+  const fontSize = getGlobal(new sass.types.String("CONFIG.EXAMPLE_FONT_SIZE"));
   expect(height).toMatchInlineSnapshot(`
 SassNumber {
   "dartValue": SingleUnitSassNumber0 {
