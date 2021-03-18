7f192bb4a8c67f2286017b560cfd1afaad07c4c1
avallete <avallete@student.42.fr>
2021-03-18 12:19:36 +0100

    test(sass): refactor sass test snapshot to match with dartSass SassNumber

--- __tests__/sass.test.ts
+++ __tests__/sass.test.ts
@@ -5,6 +5,22 @@ it("can expose data to SASS", () => {
   const getGlobal = getGlobalFrom({ CONFIG });
   const height = getGlobal({ getValue: () => "CONFIG.EXAMPLE_HEIGHT" });
   const fontSize = getGlobal({ getValue: () => "CONFIG.EXAMPLE_FONT_SIZE" });
-  expect(height).toMatchInlineSnapshot(`SassNumber {}`);
-  expect(fontSize).toMatchInlineSnapshot(`SassNumber {}`);
+  expect(height).toMatchInlineSnapshot(`
+SassNumber {
+  "dartValue": SingleUnitSassNumber0 {
+    "_single_unit$_unit": "px",
+    "asSlash": null,
+    "value": 200,
+  },
+}
+`);
+  expect(fontSize).toMatchInlineSnapshot(`
+SassNumber {
+  "dartValue": SingleUnitSassNumber0 {
+    "_single_unit$_unit": "em",
+    "asSlash": null,
+    "value": 2,
+  },
+}
+`);
 });
