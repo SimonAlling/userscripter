8fcf2fdf820cb65345d10baa8ad74381ea2c288e
avallete <avallete@student.42.fr>
2021-04-08 10:55:04 +0200

    test(sass): remove unused sass tests in programatic context
    add another test for custom getter function name

--- __tests__/sass.test.ts
+++ __tests__/sass.test.ts
@@ -7,65 +7,29 @@ import * as CONFIG from "./config-example";
 
 const defaultSassVariableGetter = DEFAULT_BUILD_CONFIG({ rootDir: "", id: "", now: new Date() }).sassVariableGetter;
 
-describe("sass in programmatic context", () => {
-  it("can expose data to SASS", () => {
-    const getGlobal = getGlobalFrom({ CONFIG });
-    const height = getGlobal(new sass.types.String("CONFIG.EXAMPLE_HEIGHT"));
-    const fontSize = getGlobal(new sass.types.String("CONFIG.EXAMPLE_FONT_SIZE"));
-    expect(height).toMatchInlineSnapshot(`
-  SassNumber {
-    "dartValue": SingleUnitSassNumber0 {
-      "_single_unit$_unit": "px",
-      "asSlash": null,
-      "value": 200,
-    },
-  }
-  `);
-    expect(fontSize).toMatchInlineSnapshot(`
-  SassNumber {
-    "dartValue": SingleUnitSassNumber0 {
-      "_single_unit$_unit": "em",
-      "asSlash": null,
-      "value": 2,
-    },
-  }
-  `);
-  });
-
-  it("can expose deep nested data to SASS", () => {
-    const deepNestedConfig = {
-      deep: {
-        nested: {
-          CONFIG
-        }
-      }
-    };
-    const getGlobal = getGlobalFrom(deepNestedConfig);
-    const height = getGlobal(new sass.types.String("deep.nested.CONFIG.EXAMPLE_HEIGHT"));
-    expect(height).toMatchInlineSnapshot(`
-  SassNumber {
-    "dartValue": SingleUnitSassNumber0 {
-      "_single_unit$_unit": "px",
-      "asSlash": null,
-      "value": 200,
-    },
-  }
-  `);
-  });
-
-  it("should throw an error when trying to get undefined variable", () => {
-    const getGlobal = getGlobalFrom({ CONFIG });
-    expect(() => getGlobal(new sass.types.String("CONFIG.NON_EXISTENT")))
-      .toThrowError(`Unknown global: 'CONFIG.NON_EXISTENT' (failed on 'NON_EXISTENT')`);
-  });
-});
-
-describe("sass in scss context", () => {
+describe("sass global variables getter", () => {
   it("can call getGlobal inside scss context with default sassVariableGetter", () => {
     const getGlobal = getGlobalFrom({ CONFIG });
     const scssTemplate = `div { min-height: #{getGlobal("CONFIG.EXAMPLE_HEIGHT")} }`;
     const encodedFunctionName = withDartSassEncodedParameters(defaultSassVariableGetter, getGlobal);
     expect(encodedFunctionName).toBe(`${defaultSassVariableGetter}($x0)`);
+    const sassRenderConfig: sass.Options = {
+      data: scssTemplate,
+      outputStyle: "compressed",
+      functions: {
+        [encodedFunctionName]: getGlobal
+      }
+    };
+    const result = sass.renderSync(sassRenderConfig);
+    expect(result.css.toString()).toBe(`div{min-height:200px}`);
+  });
+
+  it("can customize getGlobal to another function name", () => {
+    const getGlobal = getGlobalFrom({ CONFIG });
+    const customSassVariableGetter = "getFoo";
+    const scssTemplate = `div { min-height: #{getFoo("CONFIG.EXAMPLE_HEIGHT")} }`;
+    const encodedFunctionName = withDartSassEncodedParameters(customSassVariableGetter, getGlobal);
+    expect(encodedFunctionName).toBe(`${customSassVariableGetter}($x0)`);
     const sassRenderConfig: sass.Options = {
       data: scssTemplate,
       outputStyle: "compressed",
