7866016e8c6e736c101e3f88ea9e272c2f62ab06
Simon Alling <alling.simon@gmail.com>
2021-04-17 16:47:15 +0200

    Clarify tests

--- __tests__/sass.test.ts
+++ __tests__/sass.test.ts
@@ -5,14 +5,15 @@ import { DEFAULT_BUILD_CONFIG } from "../src/build/internal/webpack";
 
 import * as CONFIG from "./config-example";
 
-const defaultSassVariableGetter = DEFAULT_BUILD_CONFIG({ rootDir: "", id: "", now: new Date() }).sassVariableGetter;
+const actualDefaultVariableGetter = DEFAULT_BUILD_CONFIG({ rootDir: "", id: "", now: new Date() }).sassVariableGetter;
+const expectedDefaultVariableGetter = "getGlobal";
 
-describe("sass global variables getter", () => {
-  it("can call getGlobal inside scss context with default sassVariableGetter", () => {
+describe("SASS variable getter", () => {
+  it("can be called using the expected default name", () => {
     const getGlobal = getGlobalFrom({ CONFIG });
-    const scssTemplate = `div { min-height: #{getGlobal("CONFIG.EXAMPLE_HEIGHT")} }`;
-    const encodedFunctionName = withDartSassEncodedParameters(defaultSassVariableGetter, getGlobal);
-    expect(encodedFunctionName).toBe(`${defaultSassVariableGetter}($x0)`);
+    const scssTemplate = `div { min-height: #{${expectedDefaultVariableGetter}("CONFIG.EXAMPLE_HEIGHT")} }`;
+    const encodedFunctionName = withDartSassEncodedParameters(actualDefaultVariableGetter, getGlobal);
+    expect(encodedFunctionName).toBe(`${actualDefaultVariableGetter}($x0)`);
     const sassRenderConfig: sass.Options = {
       data: scssTemplate,
       outputStyle: "compressed",
@@ -24,12 +25,10 @@ describe("sass global variables getter", () => {
     expect(result.css.toString()).toBe(`div{min-height:200px}`);
   });
 
-  it("can customize getGlobal to another function name", () => {
+  it("can be called using the actual default name", () => {
     const getGlobal = getGlobalFrom({ CONFIG });
-    const customSassVariableGetter = "getFoo";
-    const scssTemplate = `div { min-height: #{getFoo("CONFIG.EXAMPLE_HEIGHT")} }`;
-    const encodedFunctionName = withDartSassEncodedParameters(customSassVariableGetter, getGlobal);
-    expect(encodedFunctionName).toBe(`${customSassVariableGetter}($x0)`);
+    const scssTemplate = `div { min-height: #{${actualDefaultVariableGetter}("CONFIG.EXAMPLE_HEIGHT")} }`;
+    const encodedFunctionName = withDartSassEncodedParameters(actualDefaultVariableGetter, getGlobal);
     const sassRenderConfig: sass.Options = {
       data: scssTemplate,
       outputStyle: "compressed",
@@ -41,27 +40,45 @@ describe("sass global variables getter", () => {
     expect(result.css.toString()).toBe(`div{min-height:200px}`);
   });
 
-  it("getGlobal should throw an error if the variable doesn't exist", () => {
+  it("can be called using a custom name", () => {
     const getGlobal = getGlobalFrom({ CONFIG });
-    const scssTemplate = `div { min-height: #{getGlobal("CONFIG.NON_EXISTENT")} }`;
+    const variableGetter = "getFoo";
+    const scssTemplate = `div { min-height: #{${variableGetter}("CONFIG.EXAMPLE_HEIGHT")} }`;
+    const encodedFunctionName = withDartSassEncodedParameters(variableGetter, getGlobal);
     const sassRenderConfig: sass.Options = {
       data: scssTemplate,
       outputStyle: "compressed",
       functions: {
-        [withDartSassEncodedParameters(defaultSassVariableGetter, getGlobal)]: getGlobal
+        [encodedFunctionName]: getGlobal
+      }
+    };
+    const result = sass.renderSync(sassRenderConfig);
+    expect(result.css.toString()).toBe(`div{min-height:200px}`);
+  });
+
+  it("throws an error if the variable doesn't exist", () => {
+    const getGlobal = getGlobalFrom({ CONFIG });
+    const variableGetter = expectedDefaultVariableGetter;
+    const scssTemplate = `div { min-height: #{${variableGetter}("CONFIG.NON_EXISTENT")} }`;
+    const sassRenderConfig: sass.Options = {
+      data: scssTemplate,
+      outputStyle: "compressed",
+      functions: {
+        [withDartSassEncodedParameters(variableGetter, getGlobal)]: getGlobal
       }
     };
     expect(() => sass.renderSync(sassRenderConfig)).toThrowError(`Unknown global: 'CONFIG.NON_EXISTENT' (failed on 'NON_EXISTENT')`);
   });
 
-  it("sass should throw error if invalid argument type is passed to getGlobal", () => {
+  it("throws an error if passed a non-string argument", () => {
     const getGlobal = getGlobalFrom({ CONFIG });
-    const scssTemplate = `div { min-height: #{getGlobal(42)} }`;
+    const variableGetter = expectedDefaultVariableGetter;
+    const scssTemplate = `div { min-height: #{${variableGetter}(42)} }`;
     const sassRenderConfig: sass.Options = {
       data: scssTemplate,
       outputStyle: "compressed",
       functions: {
-        [withDartSassEncodedParameters(defaultSassVariableGetter, getGlobal)]: getGlobal
+        [withDartSassEncodedParameters(variableGetter, getGlobal)]: getGlobal
       }
     };
     expect(() => sass.renderSync(sassRenderConfig)).toThrowError(`Error: Expected a string as argument, but saw: 42`);
