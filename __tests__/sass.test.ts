b4fd4f6c3556f9880019bce26322ba043f275fad
Andrew Valleteau <avallete@users.noreply.github.com>
2021-04-21 08:39:07 +0200

    refactor: Replace node-sass with sass (#45)
    [Node Sass has been deprecated in favor of Dart Sass.][libsass]
    
    For this project in particular, Node Sass is the cause of lengthy
    compilations when running `npm ci`, annoying GYP errors and [multiple
    security vulnerabilities][snyk].
    
    [libsass]: https://sass-lang.com/blog/libsass-is-deprecated
    [snyk]: https://snyk.io/test/npm/userscripter/1.4.0
    
    When we first tried to migrate in #29, it turned out that ["just
    [replacing] `node-sass` in [our] `package.json` file with
    `sass`"][migrate] didn't work, because doing so broke our mechanism for
    accessing JavaScript values from within SCSS (i.e. `getGlobal`). For
    example, tests would fail like so:
    
        can expose data to SASS
    
        expect(received).toMatchInlineSnapshot(snapshot)
    
        Snapshot name: `can expose data to SASS 1`
    
        Snapshot: SassNumber {}
        Received: undefined
    
    [migrate]: https://sass-lang.com/blog/libsass-is-deprecated#how-do-i-migrate
    
    We tried to fix this by upgrading packages (e.g. `node-sass-utils` to
    version 1.1.3 and `sass-loader` to 11.0.1), but that would cause build
    errors in dependent userscripts like [Better SweClockers][bsc] (e.g.
    `SassError: Only 0 arguments allowed, but 1 was passed.` at `getGlobal`
    call sites). As far as we could tell, we wouldn't be able to fix that by
    upgrading dependencies, at least not without forcing dependent projects
    to upgrade to webpack 5.
    
    [bsc]: https://github.com/simonalling/better-sweclockers
    
    The solution we finally arrived at was to adapt `getGlobalFrom` to work
    with `sass` instead of `node-sass` and, in the webpack config we
    generate for dependent projects, make sure the parameters of the SASS
    variable getter are properly Dart Sass-encoded – conceptually:
    
    ```diff
     sassOptions: {
         functions: {
    -        ["getGlobal"]: getGlobal,
    +        ["getGlobal($x0)"]: getGlobal,
         },
     },
    ```
    
    We decided that this is _not_ a breaking change, because although
    dependent projects using the SASS variable getter in JavaScript (i.e.
    not in SCSS) may break, we don't consider that a supported use case (and
    we don't know of any such projects). Dependent projects using
    `getGlobal` as intended (i.e. in SCSS) shouldn't be affected. In order
    to confirm this, we have built, tested and used Better SweClockers (as
    of [`f01d834`][bsc-commit]) based on this PR without any issues.
    
    [bsc-commit]: https://github.com/SimonAlling/better-sweclockers/commit/f01d8343e0f6b81a4a987c620c2a1ed6c1499edf
    
    Co-authored-by: Simon Alling <alling.simon@gmail.com>

--- __tests__/sass.test.ts
+++ __tests__/sass.test.ts
@@ -1,11 +1,86 @@
-import { getGlobalFrom } from "../src/build/internal/sass";
+import sass from "sass";
+
+import { getGlobalFrom, withDartSassEncodedParameters } from "../src/build/internal/sass";
+import { DEFAULT_BUILD_CONFIG } from "../src/build/internal/webpack";
 
 import * as CONFIG from "./config-example";
 
-it("can expose data to SASS", () => {
-  const getGlobal = getGlobalFrom({ CONFIG });
-  const height = getGlobal({ getValue: () => "CONFIG.EXAMPLE_HEIGHT" });
-  const fontSize = getGlobal({ getValue: () => "CONFIG.EXAMPLE_FONT_SIZE" });
-  expect(height).toMatchInlineSnapshot(`SassNumber {}`);
-  expect(fontSize).toMatchInlineSnapshot(`SassNumber {}`);
+const actualDefaultVariableGetter = DEFAULT_BUILD_CONFIG({ rootDir: "", id: "", now: new Date() }).sassVariableGetter;
+const expectedDefaultVariableGetter = "getGlobal";
+
+describe("SASS variable getter", () => {
+  it("can be called using the expected default name", () => {
+    const getGlobal = getGlobalFrom({ CONFIG });
+    const scssTemplate = `div { min-height: #{${expectedDefaultVariableGetter}("CONFIG.EXAMPLE_HEIGHT")} }`;
+    const encodedFunctionName = withDartSassEncodedParameters(actualDefaultVariableGetter, getGlobal);
+    expect(encodedFunctionName).toBe(`${actualDefaultVariableGetter}($x0)`);
+    const sassRenderConfig: sass.Options = {
+      data: scssTemplate,
+      outputStyle: "compressed",
+      functions: {
+        [encodedFunctionName]: getGlobal,
+      },
+    };
+    const result = sass.renderSync(sassRenderConfig);
+    expect(result.css.toString()).toBe(`div{min-height:200px}`);
+  });
+
+  it("can be called using the actual default name", () => {
+    const getGlobal = getGlobalFrom({ CONFIG });
+    const scssTemplate = `div { min-height: #{${actualDefaultVariableGetter}("CONFIG.EXAMPLE_HEIGHT")} }`;
+    const encodedFunctionName = withDartSassEncodedParameters(actualDefaultVariableGetter, getGlobal);
+    const sassRenderConfig: sass.Options = {
+      data: scssTemplate,
+      outputStyle: "compressed",
+      functions: {
+        [encodedFunctionName]: getGlobal,
+      },
+    };
+    const result = sass.renderSync(sassRenderConfig);
+    expect(result.css.toString()).toBe(`div{min-height:200px}`);
+  });
+
+  it("can be called using a custom name", () => {
+    const getGlobal = getGlobalFrom({ CONFIG });
+    const variableGetter = "getFoo";
+    const scssTemplate = `div { min-height: #{${variableGetter}("CONFIG.EXAMPLE_HEIGHT")} }`;
+    const encodedFunctionName = withDartSassEncodedParameters(variableGetter, getGlobal);
+    const sassRenderConfig: sass.Options = {
+      data: scssTemplate,
+      outputStyle: "compressed",
+      functions: {
+        [encodedFunctionName]: getGlobal,
+      },
+    };
+    const result = sass.renderSync(sassRenderConfig);
+    expect(result.css.toString()).toBe(`div{min-height:200px}`);
+  });
+
+  it("throws an error if the variable doesn't exist", () => {
+    const getGlobal = getGlobalFrom({ CONFIG });
+    const variableGetter = expectedDefaultVariableGetter;
+    const scssTemplate = `div { min-height: #{${variableGetter}("CONFIG.NON_EXISTENT")} }`;
+    const sassRenderConfig: sass.Options = {
+      data: scssTemplate,
+      outputStyle: "compressed",
+      functions: {
+        [withDartSassEncodedParameters(variableGetter, getGlobal)]: getGlobal,
+      },
+    };
+    expect(() => sass.renderSync(sassRenderConfig)).toThrowError(`Unknown global: 'CONFIG.NON_EXISTENT' (failed on 'NON_EXISTENT')`);
+  });
+
+  it("throws an error if passed a non-string argument", () => {
+    const getGlobal = getGlobalFrom({ CONFIG });
+    const variableGetter = expectedDefaultVariableGetter;
+    const scssTemplate = `div { min-height: #{${variableGetter}(42)} }`;
+    const sassRenderConfig: sass.Options = {
+      data: scssTemplate,
+      outputStyle: "compressed",
+      functions: {
+        [withDartSassEncodedParameters(variableGetter, getGlobal)]: getGlobal,
+      },
+    };
+    expect(() => sass.renderSync(sassRenderConfig)).toThrowError(`Error: Expected a string as argument, but saw: 42`);
+  });
 });
