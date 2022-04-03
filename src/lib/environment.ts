0af9ff679eb6abc6ea8ca85a194ebc104470ccd9
Simon Alling <alling.simon@gmail.com>
2022-04-03 21:28:47 +0200

    refactor: Move comment about condition type

--- src/lib/environment.ts
+++ src/lib/environment.ts
@@ -1,3 +1,8 @@
+/*
+This type is chosen so that side-effects can be deferred from the time of declaring operations to when they are executed.
+A prime example is if a condition should be based on the content of document.head:
+In some scenarios, e.g. a WebExtension running in Google Chrome, document.head is null when the operations are declared.
+*/
 export type Condition = (w: Window) => boolean;
 
 export const ALWAYS = () => true;
