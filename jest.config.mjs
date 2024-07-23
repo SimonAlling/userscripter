0e8696f8d4c1e844dcc69ac8d8eee2039ccf8765
Simon Alling <alling.simon@gmail.com>
2024-07-23 22:54:58 +0200

    build: Move Jest configuration to its own file

---
+++ jest.config.mjs
@@ -0,0 +1,11 @@
+export default {
+  "testEnvironment": "jsdom",
+  "transform": {
+    "^.+\\.ts$": "ts-jest"
+  },
+  "testRegex": ".+\\.test\\.ts$",
+  "moduleFileExtensions": [
+    "ts",
+    "js"
+  ]
+};
